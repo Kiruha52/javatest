0ca33e9626eb3d5bd7dd662c3c42ec1320698a66
Кирилл Фарафошин <kirill@MacBook-Air-Kirill-2.local>
2026-09-12 14:58:11 +0300

    classwork

--- main.java
+++ main.java
@@ -1,6 +1,88 @@
-import java.util.Scanner;
-public class main {
+/* import java.util.Scanner;
+public class main{
 	public static void main(String[] args){
-		System.out.print("123123123");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		if (a+b > c && a+c > b && b+c > a){
+			System.out.println("Да");
+		}
+		else{
+			System.out.println("нет");
+		}
+
 	}
 }
+import java.util.Scanner;
+public class main{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		double x = scan.nextDouble();
+		double y = scan.nextDouble();
+		int n = scan.nextInt();
+		if(x <= (double)n /2 && x >= ((double)n * (-1))/2 && y <= (double)n /2 && y >= ((double)n * (-1))/2){
+			System.out.println("попал");
+		}
+		else{
+			System.out.println("не попал");
+		}
+
+	}
+}
+
+import java.util.Scanner;
+public class main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		double x = sc.nextDouble();
+		double y = sc.nextDouble();
+		int n = sc.nextInt();
+		if(x*x + y*y <= n*n){
+			System.out.println("попал");
+		}
+		else{
+			System.out.println("не попал");
+		}
+
+	}
+}
+
+import java.util.Scanner;
+public class main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		if(a%10 == a/100){
+			System.out.println("палиндром");
+		}
+		else{
+			System.out.println("не палиндром");
+		}
+
+	}
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
